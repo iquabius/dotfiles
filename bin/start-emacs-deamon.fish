14508a97f2784afea3606a7564c57be8a8a0ccfd
Josias Iquabius <iquabius@gmail.com>
2020-09-17 21:38:28 -0400

    Document reason to use empty XMODIFIERS when starting Emacs daemon

--- bin/start-emacs-deamon.fish
+++ bin/start-emacs-deamon.fish
@@ -1,5 +1,7 @@
 #! /usr/bin/fish
 
+# Empty XMODIFIERS is needed to fix "<dead_tilde> is undefined" on Ubuntu
+# https://www.emacswiki.org/emacs/DeadKeys
 /usr/bin/env XLIB_SKIP_ARGB_VISUALS=1 XMODIFIERS= /usr/bin/emacs --daemon
 
 notify-send -i emacs "GNU Emacs daemon started!"
