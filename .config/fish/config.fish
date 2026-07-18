3409266511211bdef93f92f53212e22cfcb16a18
Josias Iquabius <iquabius@gmail.com>
2026-07-17 21:09:00 -0400

    Add LM Studio CLI to PATH in Fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -203,3 +203,7 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/josiasbusiquia/.lmstudio/bin
+# End of LM Studio CLI section
